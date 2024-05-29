e53f94d0db05abbbe4279e1557de86d6caf533ab
SoraYuuEU <azdiva3531@gmail.com>
2024-05-29 19:48:22 +0700

    Update autoplay.lua

--- autoplay.lua
+++ autoplay.lua
@@ -36,7 +36,7 @@ getgenv().autoBoost = { -- this will use boosts when it runs out
 }
 
 getgenv().autoMail = {
-    enabled = false,          -- for gems
+    enabled = true,          -- for gems
     sendTroopsEnabled = false,
     idToSendTo = 5456148550,  -- for gems
     idToSendTo2 = 5456140552, -- this is for troops
@@ -46,7 +46,7 @@ getgenv().autoMail = {
         ["Basic"] = false,
         ["Epic"] = false,
         ["Mythic"] = true,
-        ["Uncommon"] = false,
+        ["Uncommon"] = true,
         ["Rare"] = false,
         ["Exclusive"] = true,
         ["Godly"] = true
@@ -846,7 +846,7 @@ if game.PlaceId == 13775256536 then
         tryClaimMail()
     end)
 
-    if readfile(game.Players.LocalPlayer.Name .. "level.txt") == "80" then
+    if readfile(game.Players.LocalPlayer.Name .. "level.txt") == "40" then
         task.spawn(function()
             trySendAllGems()
         end)
@@ -910,7 +910,7 @@ if game.PlaceId == 13775256536 then
 
     G2L["4"]["Text"] = [[Status: Joining map: ]] .. getgenv().autoPlay.map;
     print("Lobby | Joining map: " .. getgenv().autoPlay.map)
-    if readfile(game.Players.LocalPlayer.Name .. "level.txt") ~= "80" then
+    if readfile(game.Players.LocalPlayer.Name .. "level.txt") ~= "40" then
         startGame(getgenv().autoPlay.map, 1)
     end
 elseif game.PlaceId == 14082129854 then
